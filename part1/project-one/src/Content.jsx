e2eb8cb9c40cb68459804f2c5deb98a109f788c9
angelbarros14 <social.angelbarros@gmail.com>
2025-04-07 13:42:56 +0800

    exercise 1.2

--- part1/project-one/src/Content.jsx
+++ part1/project-one/src/Content.jsx
@@ -1,15 +1,11 @@
+import Part from './Part'
+
 const Content = (props) => {
     return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2}/>
+      <Part part={props.part3} exercise={props.exercise3}/>
     </>
       
     )
