0cb3f114895ffc65a8e151c2772177e7e71e4b57
angelbarros14 <social.angelbarros@gmail.com>
2025-04-09 03:06:33 +0800

    exercise 1.9

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -3,6 +3,24 @@ import Button from './Button'
 import Header from './Header'
 import Statistics from './Statistics'
 
+const StatisticsLine = ({ good, neutral, bad }) => {
+  let sum = good + bad + neutral
+  if (sum === 0) {
+    return (
+      <p>No feedback given.</p>
+    )
+  }
+  return (
+    <>
+      <Statistics text='good' result={good}/>
+      <Statistics text='neutral' result={neutral}/>
+      <Statistics text='bad' result={bad}/>
+      <Statistics text='average' result={((good * 1) + (neutral * 0) + (bad * -1)) / sum}/>
+      <Statistics text='positive' result={`${(good / sum) * 100}%`}/>
+    </>
+  )
+}
+
 const App = () => {
   const [ good, setGood ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
@@ -23,10 +41,6 @@ const App = () => {
     setBad(newValue)
   }
 
-  let sum = good + bad + neutral
-  const average = sum == 0 ? 0 : ((good * 1) + (neutral * 0) + (bad * -1)) / sum
-  const positive = sum == 0 ? 0: `${(good / sum) * 100}%`
-
   return (
     <>
       <Header header='Give Feedback'/>
@@ -34,11 +48,7 @@ const App = () => {
       <Button onClick={setValueNeutral(neutral + 1)} text='neutral' />
       <Button onClick={setValueBad(bad + 1)} text='bad' />
       <Header header='Statistics' />
-      <Statistics text='good' result={good}/>
-      <Statistics text='neutral' result={neutral}/>
-      <Statistics text='bad' result={bad}/>
-      <Statistics text='average' result={average}/>
-      <Statistics text='positive' result={positive}/>
+      <StatisticsLine good={good} neutral={neutral} bad={bad}/>
     </>
   )
 }
