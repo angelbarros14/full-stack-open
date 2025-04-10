59ad02ba5d688d169a2f3cae9fd9a1c7933f33ab
angelbarros14 <social.angelbarros@gmail.com>
2025-04-10 16:22:34 +0800

    refactor

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -28,27 +28,12 @@ const App = () => {
   const [ neutral, setNeutral ] = useState(0)
   const [ bad, setBad ] = useState(0)
 
-  const setValueGood = newValue => () => {
-    // console.log('value good', newValue)
-    setGood(newValue)
-  }
-
-  const setValueNeutral = newValue => () => {
-    // console.log('value neutral', newValue)
-    setNeutral(newValue)
-  }
-
-  const setValueBad = newValue => () => {
-    // console.log('value bad', newValue)
-    setBad(newValue)
-  }
-
   return (
     <>
       <Header header='Give Feedback'/>
-      <Button onClick={setValueGood(good + 1)} text='good' />
-      <Button onClick={setValueNeutral(neutral + 1)} text='neutral' />
-      <Button onClick={setValueBad(bad + 1)} text='bad' />
+      <Button onClick={() => setGood (good + 1)} text='good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='bad' />
       <Header header='Statistics' />
       <StatisticsLine good={good} neutral={neutral} bad={bad}/>
     </>
