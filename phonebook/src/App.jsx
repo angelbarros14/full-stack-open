25f868227b1e0b43c90e4c631ef368a87485bb2c
angelbarros14 <social.angelbarros@gmail.com>
2025-04-11 22:40:58 +0800

    exercise 2.9

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import Header from './component/Header'
 
 const App = () => {
   const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filterName, setFilterName ] = useState('')
 
   const addInfo = (event) => {
     event.preventDefault()
@@ -28,7 +30,7 @@ const App = () => {
     setPersons(prev => prev.concat(nameObject))
     setNewName('')
     setNewNumber('')
-    // delay on printing updated state
+    // ***delay on printing updated state***
     console.log('after', persons)
   }
 
@@ -40,9 +42,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilterName(event.target.value)
+  }
+
+  // access filter name:
+    // show the list depending on the filtered name
+    // access the persons list (?), and display the names 
+  const filter = persons.filter(person => person.name.includes(filterName.toLowerCase()))
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <Header text='Phonebook' />
+      <div>filter shown with <input value={filterName} onChange={handleFilter}/> </div>
+      <Header text='Add new' />
       <form onSubmit={addInfo}>
         <div> name: <input value={newName} onChange={handleName} /> </div>
         <div> number: <input value={newNumber} onChange={handleNumber}/> </div>
@@ -50,8 +63,8 @@ const App = () => {
           <button type='submit'>add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map(person => <div key={person.id}> {person.name} {person.number}</div>)}
+      <Header text='Numbers' />
+      {filter.map(person => <div key={person.id}> {person.name} {person.number}</div>)}
     </div>
   )
 }
