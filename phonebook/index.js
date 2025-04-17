e99b49d47dc1a994882dd1b1d9d7e7f8b9876df9
angelbarros14 <social.angelbarros@gmail.com>
2025-04-17 16:35:07 +0800

    exercise 3.2

--- phonebook/index.js
+++ phonebook/index.js
@@ -24,6 +24,14 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>`
+    )
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`server running on port ${PORT}`)
