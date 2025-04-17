fd14405ca26d58245e8992831092ccb9f1588e7a
angelbarros14 <social.angelbarros@gmail.com>
2025-04-17 21:18:08 +0800

    exercise 3.7

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+    console.log('Method', request.method)
+    console.log('Path', request.path)
+    console.log('Body', request.body)
+    console.log('---')
+    next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
+app.use(morgan('combined'))
 
 let persons = [
       {
@@ -85,6 +96,14 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({
+        error: 'unknown endpoint'
+    })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
