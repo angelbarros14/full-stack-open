962ea1006ce5025b5aec46ff5842eed9534be318
angelbarros14 <social.angelbarros@gmail.com>
2025-04-30 23:05:51 +0800

    add

--- bloglist/backend/tests/reverse.test.js
+++ bloglist/backend/tests/reverse.test.js
@@ -1,22 +1,22 @@
-// const { test, describe } = require('node:test')
-// const assert = require('node:assert')
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
 
-// const reverse = require('../utils/for_testing').reverse
+const reverse = require('../utils/for_testing').reverse
 
 
-// describe('reverse', () => {
-//     test('reverse of a', () => {
-//         assert.strictEqual(reverse('a'), 'a')
-//     })
+describe('reverse', () => {
+    test('reverse of a', () => {
+        assert.strictEqual(reverse('a'), 'a')
+    })
 
-//     test('reverse of react', () => {
-//         assert.strictEqual(reverse('react'), 'tcaer')
-//     })
+    test('reverse of react', () => {
+        assert.strictEqual(reverse('react'), 'tcaer')
+    })
 
-//     test('reverse of saippuakauppias', () => {
-//         assert.strictEqual(reverse('saippuakauppias'), 'saippuakauppias')
-//     })       
-// })
+    test('reverse of saippuakauppias', () => {
+        assert.strictEqual(reverse('saippuakauppias'), 'saippuakauppias')
+    })       
+})
 
 
 
