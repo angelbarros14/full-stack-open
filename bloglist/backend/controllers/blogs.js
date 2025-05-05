5652c04779c386675080979e908edfc1a6cbad42
angelbarros14 <social.angelbarros@gmail.com>
2025-05-05 18:32:08 +0800

    user auth add & delete blog

--- bloglist/backend/controllers/blogs.js
+++ bloglist/backend/controllers/blogs.js
@@ -1,8 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
 const jwt = require('jsonwebtoken')
-const { usersInDb } = require('../tests/test_helper')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -18,14 +17,15 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const body = request.body
-  // returns username and id field
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid'})
-  }
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
+
+  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  // if (!decodedToken.id) {
+  //   return response.status(401).json({ error: 'token invalid'})
+  // }
+  // const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -41,16 +41,15 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  // fetch blog from the database
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
   const blog = await Blog.findById(request.params.id)
-  // get the id from the jwt token
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const user = request.user 
+  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
   console.log('blog id:', blog.user.toString())
-  console.log('user id:', decodedToken.id.toString())
+  console.log('user id:', user.id.toString())
 
   // compare the jwt id token and who created the blog (blog id)
-  if (blog.user.toString() === decodedToken.id.toString()) {
+  if (blog.user.toString() === user.id.toString()) {
     await Blog.findByIdAndDelete(request.params.id)
     return response.status(204).end()
   } else {
--- bloglist/backend/utils/middleware.js
+++ bloglist/backend/utils/middleware.js
@@ -1,6 +1,8 @@
 // custom middleware
 
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -23,7 +25,7 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-const tokenExtractor = (request, response, next)=> {
+const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   console.log('authorization header', authorization)
   if (authorization && authorization.startsWith('Bearer ')) {
@@ -34,6 +36,16 @@ const tokenExtractor = (request, response, next)=> {
   next()
 }
 
+// // find out the identity of the user who is doing the operation
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid'})
+  }
+  request.user = await User.findById(decodedToken.id)
+  next()
+}
+
 module.exports = {
-    errorHandler, unknownEndpoint, tokenExtractor
+    errorHandler, unknownEndpoint, tokenExtractor, userExtractor
 }
